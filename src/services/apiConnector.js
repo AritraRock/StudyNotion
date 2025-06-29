0024eb89992f6c5fa3ccced2ff347a02b2dca576
Aritra <aritrabts@gmail.com>
2025-06-29 22:33:17 +0530

    solve otp issue

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({});
+// const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export const axiosInstance = axios.create({
+  // baseURL: BASE_URL,
+});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
+  // console.log("✅ Calling API:", url);
   return axiosInstance({
     method: `${method}`,
     url: `${url}`,
