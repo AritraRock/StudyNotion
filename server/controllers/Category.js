5c3164f9fa21d8284c60186f5c225d0cfc177049
Aritra <aritrabts@gmail.com>
2025-07-06 23:59:00 +0530

    send category submission data to frontend

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -18,7 +18,11 @@ exports.createCategory = async (req, res) => {
     console.log(CategorysDetails)
     return res.status(200).json({
       success: true,
-      message: "Categorys Created Successfully",
+      message: "Category Created Successfully",
+      data:{
+        name,
+        description
+      }
     })
   } catch (error) {
     return res.status(500).json({
