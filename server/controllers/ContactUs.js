b9e96ad364562e376bf9f947831b00347c9bc7b6
Aritra <aritrabts@gmail.com>
2025-07-06 15:12:59 +0530

    add email for user's message from contact us form to owner

--- server/controllers/ContactUs.js
+++ server/controllers/ContactUs.js
@@ -1,19 +1,27 @@
 const { contactUsEmail } = require("../mail/templates/contactFormRes")
+const { contactOwnerEmail } = require("../mail/templates/contactFormToOwner")
 const mailSender = require("../utils/mailSender")
 
 exports.contactUsController = async (req, res) => {
   const { email, firstname, lastname, message, phoneNo, countrycode } = req.body
   console.log(req.body)
   try {
+    //email verification on successful submission of form to the user
     const emailRes = await mailSender(
       email,
-      "Your Data send successfully",
+      "Your Data send successfully to user",
       contactUsEmail(email, firstname, lastname, message, phoneNo, countrycode)
     )
     console.log("Email Res ", emailRes)
+    //email to owner
+    await mailSender(
+      process.env.OWNER_EMAIL,
+      "New Contact Us Form Submission",
+      contactOwnerEmail(email, firstname, lastname, message, phoneNo, countrycode)
+    )
     return res.json({
       success: true,
-      message: "Email send successfully",
+      message: "Both the emails send successfully",
     })
   } catch (error) {
     console.log("Error", error)
